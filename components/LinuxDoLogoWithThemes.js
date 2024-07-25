fda634a0f7f80066e58736c9693d307c84b2ea30
Chenyme <118253778+Chenyme@users.noreply.github.com>
2024-07-26 01:05:08 +0800

    Fixed System mode picture BUG

--- components/LinuxDoLogoWithThemes.js
+++ components/LinuxDoLogoWithThemes.js
@@ -1,13 +1,30 @@
-import Image from 'next/image'
-import {useTheme} from "nextra-theme-docs";
+import Image from 'next/image';
+import { useTheme } from 'nextra-theme-docs';
+import { useEffect, useState } from 'react';
 
 const LinuxDoLogoWithThemes = () => {
-    let src = useTheme().theme === 'light'
+    const { theme } = useTheme();
+    const [currentTheme, setCurrentTheme] = useState(theme);
+
+    useEffect(() => {
+        if (theme === 'system') {
+            const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+            const updateTheme = () => setCurrentTheme(darkModeMediaQuery.matches ? 'dark' : 'light');
+            updateTheme();
+            darkModeMediaQuery.addEventListener('change', updateTheme);
+
+            return () => darkModeMediaQuery.removeEventListener('change', updateTheme);
+        } else {
+            setCurrentTheme(theme);
+        }
+    }, [theme]);
+
+    const src = currentTheme === 'light'
         ? '/linuxdo_logo_with_dark_text.png'
         : '/linuxdo_logo_with_light_text.png';
 
     return (
-        <Image src={src} width={220} height={100} />
+        <Image src={src} width={220} height={100} alt="LinuxDo Logo" />
     );
 }
 
