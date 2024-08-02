3573ba51e4d7b1c81651931373db86488713ffdf
Chenyme <118253778+Chenyme@users.noreply.github.com>
2024-08-02 14:02:43 +0800

    updated wiki o((>ω< ))o！

---
+++ service/web/linuxdoUserDataService.js
@@ -0,0 +1,48 @@
+import httpService from "./httpService";
+
+// fetch() on console to see the full response
+export async function retrieveUserJson(username) {
+    try {
+        const response = await httpService.get('/api/linuxdo/users/' + username + '.json');
+        return response;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export async function retrieveUserData(username) {
+    const json = await retrieveUserJson(username);
+    return json.user;
+};
+
+// sth: eg. "user.id", "user.name", "user.avatar_template", etc.
+export async function retrieveUserSth(username, sth) {
+    const user = await retrieveUserData(username);
+    return eval(sth);
+};
+
+export async function retrieveSimplifiedUser(username) {
+    const user = await retrieveUserData(username);
+    const simplifiedUser = {
+        // if suspended
+        suspended: user.suspended_till !== undefined ? true : false,// true or false
+        suspendedTill: user.suspended_till,
+        // if hidden
+        profileHidden: user.profile_hidden !== undefined ? true : false,// true or false
+
+        // can not be hidden
+        id: user.id,// fixed
+        username: user.username,// fixed
+        name: user.name,// nickname
+        avatarTemplate: user.avatar_template,// {size} can be 96 or 288
+        title: user.title,
+        // can be hidden
+        email: user.email,
+        trustLevel: user.trust_level,
+        createdAt: user.created_at,
+        gamificationScore: user.gamification_score,
+        // add more data if you need
+    };
+    return simplifiedUser;
+};
