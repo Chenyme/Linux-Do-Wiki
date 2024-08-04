946d6c5e53a317461c54b0441263baa392786e1c
WaYease <yu_weiyi@outlook.com>
2024-08-05 06:32:50 +0800

    feat(linuxdoTopicDataService.js): add linuxdoTopicDataService
    - add linuxdoTopicDataService to request topic data only with topic id

---
+++ service/web/linuxdoTopicDataService.js
@@ -0,0 +1,31 @@
+import httpService from "./httpService";
+
+// fetch() on console to see the full response
+export async function retrieveTopicJson(topicId) {
+    try {
+        const response = await httpService.get('/api/linuxdo/t/' + topicId + '.json');
+        return response;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export async function retrieveSimplifiedTopic(topicId) {
+    const json = await retrieveTopicJson(topicId);
+    const simplifiedTopic = {
+        id: json.id,// fixed
+        title: json.title,// may change
+        createdAt: json.created_at,
+        userId: json.user_id,// fixed
+        categoryId: json.category_id,// may change
+        tags: json.tags,// may none, one, or more
+        postsCount: json.posts_count,// 盖楼的层数
+        views: json.views,// 浏览量
+        likeCount: json.like_count,// 赞
+        lastPostedAt: json.last_posted_at,
+        wordCount: json.word_count,
+        participantCount: json.participant_count,
+    };
+    return simplifiedTopic;
+};
