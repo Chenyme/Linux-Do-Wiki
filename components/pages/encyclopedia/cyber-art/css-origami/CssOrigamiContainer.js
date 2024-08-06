65109702abb85b6f635aba0bf9ab862137b0bf33
WaYease <yu_weiyi@outlook.com>
2024-08-06 19:51:28 +0800

    feat: add two cyberart pages
    - add /encyclopedia/cyber-art folder
    - add Linux-Do-Wiki-component page
    - add CSS-origami page

---
+++ components/pages/encyclopedia/cyber-art/css-origami/CssOrigamiContainer.js
@@ -0,0 +1,9 @@
+const CssOrigamiContainer = ({ styles }) => {
+    return (
+        <div className={styles.body}>
+            <div className={styles.div}/>
+        </div>
+    );
+};
+
+export default CssOrigamiContainer;
