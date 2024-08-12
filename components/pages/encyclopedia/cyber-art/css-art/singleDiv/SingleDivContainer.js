2f15f29724d4d127599c9b415229a2fd2425b671
WaYease <yu_weiyi@outlook.com>
2024-08-12 21:39:05 +0800

    style: edit css art page
    - rename dir
    - add new OlympicRings component

--- components/pages/encyclopedia/cyber-art/css-art/singleDiv/SingleDivContainer.js
+++ components/pages/encyclopedia/cyber-art/css-art/singleDiv/SingleDivContainer.js
@@ -1,4 +1,4 @@
-const CssArtContainer = ({ styles }) => {
+const SingleDivContainer = ({ styles }) => {
     return (
         <div className={styles.body}>
             <div className={styles.div}/>
@@ -6,4 +6,4 @@ const CssArtContainer = ({ styles }) => {
     );
 };
 
-export default CssArtContainer;
+export default SingleDivContainer;
